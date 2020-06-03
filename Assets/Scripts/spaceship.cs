facf4dc9c7afe4c81652e01da0bb4b0ebb54edc4
Michael Hirsch <mi.hirsch.hh@gmail.com>
2020-06-03 21:30:21 +0200

    joystick added

--- Assets/Scripts/spaceship.cs
+++ Assets/Scripts/spaceship.cs
@@ -5,14 +5,17 @@ using UnityEngine;
 public class spaceship : MonoBehaviour
 {
     public float speed;
+    public Joystick joystick;
     private Rigidbody2D rb2d;
 
     void Start() {
         rb2d = GetComponent<Rigidbody2D>();
     }
     void FixedUpdate() {
-        float moveHorizontal = Input.GetAxis("Horizontal");
-        float moveVertical = Input.GetAxis("Vertical");
+        //float moveHorizontal = Input.GetAxis("Horizontal");
+        //float moveVertical = Input.GetAxis("Vertical");
+        float moveHorizontal = joystick.Horizontal;
+        float moveVertical = joystick.Vertical;
         Vector2 movement = new Vector2(moveHorizontal, moveVertical);
         rb2d.AddForce(movement*speed);
     }
