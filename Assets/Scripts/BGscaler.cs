f3bee54fa3fc39bd34476890dd579ab0b1733471
Michael Hirsch <monkdogamer@gmail.com>
2020-06-03 15:37:41 +0200

    scrolling background added

---
+++ Assets/Scripts/BGscaler.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BGscaler : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        float height = Camera.main.orthographicSize *2f;
+        float width = height * Screen.width / Screen.height;
+        transform.localScale = new Vector3(width, height, 0);
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
