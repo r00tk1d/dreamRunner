5890e48e7f3c9a2d7632f044a8a71c3c8feccdf2
Michael Hirsch <mi.hirsch.hh@gmail.com>
2020-06-04 15:02:09 +0200

    increasing obstacle spawn and random obstacle spawn

--- Assets/Scripts/ObstacleSpawner.cs
+++ Assets/Scripts/ObstacleSpawner.cs
@@ -6,48 +6,28 @@ public class ObstacleSpawner : MonoBehaviour
 {
     public GameObject[] obstacles;
     public float spawnTime = 2.0f;
-    //public List<GameObject> obstaclesToSpawn = new List<GameObject>();
-    //private int index;
+    private Vector3 spawnLocation = new Vector3(12.0f, 0.0f, 0.0f);
     // Start is called before the first frame update
     void Start()
     {
-        InvokeRepeating("InitObstacles",0.0f,spawnTime);
+        StartCoroutine(ExampleCoroutine());
+        //InvokeRepeating("InitObstacles",0.0f,spawnTime);
     }
 
-    void InitObstacles(){
-        Instantiate(obstacles[0], transform.position, Quaternion.identity);
-                
-
-
-
-        //obstaclesToSpawn.Add(obj);
-
-/**
-        for(int i = 0; i > obstacles.Length; i++) {
-            GameObject obj = Instantiate(obstacles[index], transform.position, Quaternion.identity);
-            obstaclesToSpawn.Add(obj);
-            //obstaclesToSpawn[i].SetActive(false);
-            index++;
-
-            if(index == obstacles.Length){
-                index = 0;
-            }
-        }
-        **/
-
-    }
-
-/**
-    IEnumerator SpawnRandomObstacles() {
-        yield return new WaitForSeconds(Random.Range(1.5f, 4.5f));
-        int index = Random.Range(0,obstaclesToSpawn.Count);
-
+    IEnumerator ExampleCoroutine()
+    {
+        // TODO solange der Spieler lebt
         while(true){
-            if(!obstaclesToSpawn[index].activeInHierarchy) {
-                obstaclesToSpawn[index].SetActive(true);
-                break;
+            yield return new WaitForSeconds(spawnTime);
+            Instantiate(obstacles[0], spawnLocation, Quaternion.identity);
+            
+            //increase spawntime
+            if(spawnTime > 0.6){
+                spawnTime = spawnTime - 0.03f;
             }
+
+            spawnLocation.y = Random.Range(-4.5f, 4.5f);
         }
     }
-    **/
+    
 }
