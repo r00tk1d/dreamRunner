72074590d100e38c1aff468e21eefbab41b89f71
Michael Hirsch <mi.hirsch.hh@gmail.com>
2020-06-29 12:14:25 +0200

    itembar angefangen

---
+++ Assets/Scripts/Pickup.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pickup : MonoBehaviour
+{
+    private Inventory inventory;
+    public GameObject itemButton;
+
+    private void Start()
+    {
+        inventory = GameObject.FindGameObjectWithTag("Player").GetComponent<Inventory>();
+    }
+
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        Debug.Log("crashhhh");
+        if (col.gameObject.tag == "Player")
+        {
+            for (int i = 0; i < inventory.slots.Length; i++)
+            {
+                if (inventory.isFull[i] == false)
+                {
+                    //ADD ITEM
+                    inventory.isFull[i] = true;
+                    Instantiate(itemButton, inventory.slots[i].transform, false);
+                    Destroy(gameObject);
+                    break;
+                }
+            }
+        }
+
+    }
+}
